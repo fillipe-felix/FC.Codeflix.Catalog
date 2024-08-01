125b17b2a71b07d1e67260a94ee8854f02f956fc
fillipe.felix <felipesoares_1993@hotmail.com>
2024-08-01 14:35:45 -0300

    Inciando o teste e o ciclo do TDD (Criação)

---
+++ tests/FC.Codeflix.Catalog.UnitTests/Domain/Entity/Category/CategoryTest.cs
@@ -0,0 +1,24 @@
+﻿namespace FC.Codeflix.Catalog.UnitTests.Domain.Entity.Category;
+
+public class CategoryTest
+{
+    [Fact(DisplayName = nameof(Instantiate))]
+    [Trait("Domain", "Category - Aggregates")]
+    public void Instantiate()
+    {
+        //Arrange
+        var validData = new
+        {
+            Name = "Category name",
+            Description = "Category description"
+        };
+
+        //Act
+        var category = new Category(validData.Name, validData.Description);
+
+        //Assert
+        Assert.NotNull(category);
+        Assert.Equal(validData.Name, category.Name);
+        Assert.Equal(validData.Description, category.Description);
+    }
+}
