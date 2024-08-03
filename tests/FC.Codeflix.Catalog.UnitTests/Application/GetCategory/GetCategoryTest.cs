91c32c7735bf2cee3c8fa695c27900ca38defba6
fillipe.felix <felipesoares_1993@hotmail.com>
2024-08-03 16:35:58 -0300

    Teste de exception GetCategory

--- tests/FC.Codeflix.Catalog.UnitTests/Application/GetCategory/GetCategoryTest.cs
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/GetCategory/GetCategoryTest.cs
@@ -1,4 +1,6 @@
-﻿using FluentAssertions;
+﻿using FC.Codeflix.Catalog.Application.Exceptions;
+
+using FluentAssertions;
 
 using Moq;
 
@@ -40,6 +42,31 @@ public class GetCategoryTest
         output.Description.Should().Be(exampleCategory.Description);
         output.IsActive.Should().Be(exampleCategory.IsActive);
         output.CreatedAt.Should().Be(exampleCategory.CreatedAt);
+
+        repositoryMock.Verify(x => x.Get(It.IsAny<Guid>(), It.IsAny<CancellationToken>()), Times.Once);
+    }
+    
+    [Fact(DisplayName = nameof(NotFoundExceptionWhenCategoryDoesNotExist))]
+    [Trait("Application", "GetCategory - Use Cases")]
+    public async Task NotFoundExceptionWhenCategoryDoesNotExist()
+    {
+        //Arrange
+        var repositoryMock = _fixture.GetRepositoryMock();
+        var exampleGui = Guid.NewGuid();
+        var input = new UseCases.GetCategoryInput(exampleGui);
+        var useCase = new UseCases.GetCategory(repositoryMock.Object);
+
+        repositoryMock
+            .Setup(x => x.Get(It.IsAny<Guid>(), It.IsAny<CancellationToken>()))
+            .ThrowsAsync(new NotFoundException($"Category '{exampleGui}' not found."));
+
+        //Act
+        Func<Task> task = async () => await useCase.Handle(input, CancellationToken.None);
+
+        //Assert
+        await task
+            .Should()
+            .ThrowAsync<NotFoundException>();
 
         repositoryMock.Verify(x => x.Get(It.IsAny<Guid>(), It.IsAny<CancellationToken>()), Times.Once);
     }
