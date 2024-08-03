9c220117420717ff0116a1e3af269c14eeeec686
fillipe.felix <felipesoares_1993@hotmail.com>
2024-08-03 11:26:04 -0300

    Continuando testando as exceptions no use case

--- tests/FC.Codeflix.Catalog.UnitTests/Application/CreateCategory/CreateCategoryTest.cs
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/CreateCategory/CreateCategoryTest.cs
@@ -44,6 +44,56 @@ public class CreateCategoryTest
         unitOfWorkMock.Verify(x => x.Commit(It.IsAny<CancellationToken>()), Times.Once);
     }
     
+    [Fact(DisplayName = nameof(CreateCategoryWithOnlyName))]
+    [Trait("Application", "CreateCategory - Use Cases")]
+    public async void CreateCategoryWithOnlyName()
+    {
+        //Arrange
+        var repositoryMock = _fixture.GetRepositoryMock();
+        var unitOfWorkMock = _fixture.GetUnitOfWorkMock();
+        var input = new UseCases.CreateCategoryInput(_fixture.GetValidCategoryInput().Name);
+        var useCase = new UseCases.CreateCategory(repositoryMock.Object, unitOfWorkMock.Object);
+
+        //Act
+        var output = await useCase.Handle(input, CancellationToken.None);
+
+        //Assert
+        output.Should().NotBeNull();
+        output.Id.Should().NotBeEmpty();
+        output.Name.Should().Be(input.Name);
+        output.Description.Should().BeEmpty();
+        output.IsActive.Should().BeTrue();
+        output.CreatedAt.Should().NotBeSameDateAs(default(DateTime));
+        
+        repositoryMock.Verify(x => x.Insert(It.IsAny<Category>(), It.IsAny<CancellationToken>()), Times.Once);
+        unitOfWorkMock.Verify(x => x.Commit(It.IsAny<CancellationToken>()), Times.Once);
+    }
+    
+    [Fact(DisplayName = nameof(CreateCategoryWithOnlyNameAndDescription))]
+    [Trait("Application", "CreateCategory - Use Cases")]
+    public async void CreateCategoryWithOnlyNameAndDescription()
+    {
+        //Arrange
+        var repositoryMock = _fixture.GetRepositoryMock();
+        var unitOfWorkMock = _fixture.GetUnitOfWorkMock();
+        var input = new UseCases.CreateCategoryInput(_fixture.GetValidCategoryInput().Name, _fixture.GetValidCategoryInput().Description);
+        var useCase = new UseCases.CreateCategory(repositoryMock.Object, unitOfWorkMock.Object);
+
+        //Act
+        var output = await useCase.Handle(input, CancellationToken.None);
+
+        //Assert
+        output.Should().NotBeNull();
+        output.Id.Should().NotBeEmpty();
+        output.Name.Should().Be(input.Name);
+        output.Description.Should().Be(input.Description);
+        output.IsActive.Should().BeTrue();
+        output.CreatedAt.Should().NotBeSameDateAs(default(DateTime));
+        
+        repositoryMock.Verify(x => x.Insert(It.IsAny<Category>(), It.IsAny<CancellationToken>()), Times.Once);
+        unitOfWorkMock.Verify(x => x.Commit(It.IsAny<CancellationToken>()), Times.Once);
+    }
+    
     [Theory(DisplayName = nameof(ThrowWhenCantInstantiateAggregate))]
     [Trait("Application", "CreateCategory - Use Cases")]
     [MemberData(nameof(GetInvalidInputs))]
@@ -84,6 +134,23 @@ public class CreateCategoryTest
         invalidInputLongName.Name = tooLongNameForCategory;
         invalidInputList.Add(new object[] { invalidInputLongName, "Name should be less or equal 255 characters long." });
         
+        //description nao pode ser null
+        var invalidInputLongDescriptionNull = fixture.GetValidCategoryInput();
+        invalidInputLongDescriptionNull.Description = null;
+        
+        invalidInputList.Add(new object[] { invalidInputLongDescriptionNull, "Description should not be null." });
+        
+        //description ser maior do que 10.000 caracteres
+        var invalidInputLongDescription = fixture.GetValidCategoryInput();
+        var tooLongDescriptionForCategory = fixture.Faker.Commerce.ProductDescription();
+        
+        while (tooLongDescriptionForCategory.Length < 10000)
+        {
+            tooLongDescriptionForCategory = $"{tooLongDescriptionForCategory} {fixture.Faker.Commerce.ProductDescription()}";
+        }
+        
+        invalidInputLongDescription.Description = tooLongDescriptionForCategory;
+        invalidInputList.Add(new object[] { invalidInputLongDescription, "Description should be less or equal 10000 characters long." });
         
         return invalidInputList;
     }
