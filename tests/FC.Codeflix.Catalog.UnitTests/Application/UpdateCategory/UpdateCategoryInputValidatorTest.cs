05d2f6e01876053a350cafa770b3e17051b6d073
fillipe.felix <felipesoares_1993@hotmail.com>
2024-08-06 19:59:42 -0300

    Validation do UpdateCategoryInput

---
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/UpdateCategory/UpdateCategoryInputValidatorTest.cs
@@ -0,0 +1,57 @@
+﻿using System.Globalization;
+
+using FC.Codeflix.Catalog.Application.UseCases.Category.UpdateCategory;
+
+using FluentAssertions;
+
+namespace FC.Codeflix.Catalog.UnitTests.Application.UpdateCategory;
+
+[Collection(nameof(UpdateCategoryTestFixture))]
+public class UpdateCategoryInputValidatorTest
+{
+    private readonly UpdateCategoryTestFixture _fixture;
+
+    public UpdateCategoryInputValidatorTest(UpdateCategoryTestFixture fixture)
+    {
+        _fixture = fixture;
+        
+        var cultureInfo = new CultureInfo("en-US");
+        Thread.CurrentThread.CurrentCulture = cultureInfo;
+        Thread.CurrentThread.CurrentUICulture = cultureInfo;
+    }
+
+    [Fact(DisplayName = nameof(DontValidateWhenEmptyGuid))]
+    [Trait("Application", "UpdateCategoryInputValidatorTest - Use Cases")]
+    public void DontValidateWhenEmptyGuid()
+    {
+        //Arrange
+        var input = _fixture.GetValidInput(Guid.Empty);
+        var validator = new UpdateCategoryInputValidator();
+
+        //Act
+        var validateResult = validator.Validate(input);
+
+        //Assert
+        validator.Should().NotBeNull();
+        validateResult.IsValid.Should().BeFalse();
+        validateResult.Errors.Should().HaveCount(1);
+        validateResult.Errors[0].ErrorMessage.Should().Be("'Id' must not be empty.");
+    }
+    
+    [Fact(DisplayName = nameof(ValidateWhenValid))]
+    [Trait("Application", "UpdateCategoryInputValidatorTest - Use Cases")]
+    public void ValidateWhenValid()
+    {
+        //Arrange
+        var input = _fixture.GetValidInput(Guid.NewGuid());
+        var validator = new UpdateCategoryInputValidator();
+
+        //Act
+        var validateResult = validator.Validate(input);
+
+        //Assert
+        validator.Should().NotBeNull();
+        validateResult.IsValid.Should().BeTrue();
+        validateResult.Errors.Should().BeEmpty();
+    }
+}
