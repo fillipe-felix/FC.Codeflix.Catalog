4e65015184853d6588364de75d6d2c22120f53bf
fillipe.felix <felipesoares_1993@hotmail.com>
2024-08-06 20:46:42 -0300

    Teste do use case ListCategories

---
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/ListCategories/ListCategoriesTest.cs
@@ -0,0 +1,58 @@
+﻿using FC.Codeflix.Catalog.Domain.Entity;
+
+using UseCases = FC.Codeflix.Catalog.Application.UseCases.Category.ListCategories;
+
+using Moq;
+
+namespace FC.Codeflix.Catalog.UnitTests.Application.ListCategories;
+
+[Collection(nameof(ListCategoriesTestFixture))]
+public class ListCategoriesTest
+{
+    private readonly ListCategoriesTestFixture _fixture;
+
+    public ListCategoriesTest(ListCategoriesTestFixture fixture)
+    {
+        _fixture = fixture;
+    }
+
+    [Fact(DisplayName = nameof(List))]
+    [Trait("Application", "ListCategories - Use Cases")]
+    public async Task List()
+    {
+        //Arrange
+        var categoriesExampleList = _fixture.GetExampleCategoriesList();
+        var repositoryMock = _fixture.GetRepositoryMock();
+        var input = new ListCategoriesInput(
+            page: 2,
+            perPage: 15,
+            search: "search-example",
+            sort: "name",
+            dir: SearchOrder.Asc
+        );
+        repositoryMock.Setup(x => x.Search(
+            It.Is<SearchInput>(
+                searchInput.Page == input.Page &&
+                searchInput.PerPage == input.PerPage &&
+                searchInput.Search == input.Search &&
+                searchInput.OrderBy == input.Sort &&
+                searchInput.Order == input.Dir
+            ),
+            It.IsAny<CancellationToken>()
+        )).ReturnAsync(new OutputSearch<Category>(
+            currentPage: input.Page,
+            perPage: input.PerPage,
+            items: (IReadOnlyList<Category>) categoriesExampleList,
+            total: 70
+        ));
+
+        var useCase = new UseCases.ListCategories(repositoryMock.Object);
+
+        //Act
+        var output = await useCase.Handle(input, CancellationToken.None);
+
+        //Assert
+        output.Should().NotBeNull();
+        output.Should().BeEquivalentTo(categoriesExampleList);
+    }
+}
---
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/ListCategories/ListCategoriesTestFixture.cs
@@ -0,0 +1,66 @@
+﻿using FC.Codeflix.Catalog.Domain.Entity;
+using FC.Codeflix.Catalog.Domain.Repository;
+using FC.Codeflix.Catalog.UnitTests.Common;
+
+using Moq;
+
+namespace FC.Codeflix.Catalog.UnitTests.Application.ListCategories;
+
+public class ListCategoriesTestFixture : BaseFixture
+{
+
+    public Mock<ICategoryRepository> GetRepositoryMock() => new Mock<ICategoryRepository>();
+    
+    public string GetValidCategoryName()
+    {
+        var categoryName = "";
+
+        while (categoryName.Length < 3)
+        {
+            categoryName = Faker.Commerce.Categories(1)[0];
+        }
+
+        if (categoryName.Length > 255)
+        {
+            categoryName = categoryName.Substring(0, 255);
+        }
+        
+        return categoryName;
+    }
+
+    public string GetValidCategoryDescription()
+    {
+        var categoryDescription = Faker.Commerce.ProductDescription();
+
+        if (categoryDescription.Length > 10000)
+        {
+            categoryDescription = categoryDescription.Substring(0, 10000);
+        }
+        
+        return categoryDescription;
+    }
+    
+    public Category GetValidCategory()
+    {
+        return new Category(GetValidCategoryName(), GetValidCategoryDescription());
+    }
+
+    public List<Category> GetExampleCategoriesList(int length = 10)
+    {
+        var list = new List<Category>();
+
+        for (int i = 0; i < length; i++)
+        {
+            var category = GetValidCategory();
+            list.Add(category);
+        }
+
+        return list;
+    }
+}
+
+[Collection(nameof(ListCategoriesTestFixture))]
+public class ListCategoriesTestFixtureCollection : ICollectionFixture<ListCategoriesTestFixture>
+{
+    
+}
