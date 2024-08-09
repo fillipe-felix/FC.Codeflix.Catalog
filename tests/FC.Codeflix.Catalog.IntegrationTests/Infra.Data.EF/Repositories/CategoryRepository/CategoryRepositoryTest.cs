b4444662db66a17953d2eddb87fb9b3a278dbaa6
fillipe.felix <felipesoares_1993@hotmail.com>
2024-08-09 20:26:31 -0300

    Criando teste do Insert do repositório CategoryRepository

--- tests/FC.Codeflix.Catalog.IntegrationTests/Infra.Data.EF/Repositories/CategoryRepository/CategoryRepositoryTest.cs
+++ tests/FC.Codeflix.Catalog.IntegrationTests/Infra.Data.EF/Repositories/CategoryRepository/CategoryRepositoryTest.cs
@@ -3,5 +3,30 @@
 [Collection(nameof(CategoryRepositoryTestFixture))]
 public class CategoryRepositoryTest
 {
-    
+    private readonly CategoryRepositoryTestFixture _fixture;
+
+    public CategoryRepositoryTest(CategoryRepositoryTestFixture fixture)
+    {
+        _fixture = fixture;
+    }
+
+    [Fact(DisplayName = nameof(Insert))]
+    [Trait("Integration/Infra.Data", "CategoryRepository - Repositories")]
+    public async Task Insert()
+    {
+        //Arrange
+        CodeflixCatalogDbContext dbContext = _fixture.CreateDbContext();
+        var exampleCategory = _fixture.GetExampleCategory();
+
+        var categoryRepository = new CategoryRepository(dbContext);
+
+        //Act
+        await categoryRepository.Insert(exampleCategory, CancellationToken.None);
+        await dbContext.SaveChangesAsync(CancellationToken.None);
+
+        //Assert
+        var dbCategory = await dbContext.Categories.FindAsync(exampleCategory.Id);
+        dbCategory.Should().NotBeNull();
+        dbCategory.Should().BeEquivalentTo(exampleCategory);
+    }
 }
--- tests/FC.Codeflix.Catalog.IntegrationTests/Infra.Data.EF/Repositories/CategoryRepository/CategoryRepositoryTestFixture.cs
+++ tests/FC.Codeflix.Catalog.IntegrationTests/Infra.Data.EF/Repositories/CategoryRepository/CategoryRepositoryTestFixture.cs
@@ -1,6 +1,9 @@
 ﻿using Bogus;
 
+using FC.Codeflix.Catalog.Domain.Entity;
 using FC.Codeflix.Catalog.IntegrationTests.Base;
+
+using Microsoft.EntityFrameworkCore;
 
 namespace FC.Codeflix.Catalog.IntegrationTests.Infra.Data.EF.Repositories.CategoryRepository;
 
@@ -8,6 +11,51 @@ public class CategoryRepositoryTestFixture : BaseFixture
 {
     public CategoryRepositoryTestFixture(Faker faker) : base(faker)
     {
+    }
+
+    public string GetValidCategoryName()
+    {
+        var categoryName = "";
+
+        while (categoryName.Length < 3)
+        {
+            categoryName = Faker.Commerce.Categories(1)[0];
+        }
+
+        if (categoryName.Length > 255)
+        {
+            categoryName = categoryName.Substring(0, 255);
+        }
+        
+        return categoryName;
+    }
+
+    public string GetValidCategoryDescription()
+    {
+        var categoryDescription = Faker.Commerce.ProductDescription();
+
+        if (categoryDescription.Length > 10000)
+        {
+            categoryDescription = categoryDescription.Substring(0, 10000);
+        }
+        
+        return categoryDescription;
+    }
+
+    public bool GetRandomBoolean() => new Random().NextDouble() < 0.5;
+    
+    public Category GetExampleCategory()
+    {
+        return new Category(GetValidCategoryName(), GetValidCategoryDescription(), GetRandomBoolean());
+    }
+
+    public CodeflixCatalogDbContext CreateDbContext()
+    {
+        var dbContext = new CodeflixCatalogDbContext(new DbContextOptionsBuilder<CodeflixCatalogDbContext>())
+            .UseInMemoryDatabase("integration-tests-db")
+            .Options;
+
+        return dbContext;
     }
 }
 
